f355bbf9e38b07a39c6e382af69a6dbb597f56e3
Mauricio Gabriel <souzamauricio91@hotmail.com>
2023-04-05 22:25:42 -0300

    conectando com banco de dados - postgresql

--- src/main/java/com/example/cardapio/CardapioApplication.java
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CardapioApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CardapioApplication.class, args);
 	}
 
